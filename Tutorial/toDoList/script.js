ccf0ab7b8b38c7f42228ff4d2669a692abf4f87a
maiaatala <ana.atala@unemat.br>
2022-03-04 17:45:41 -0400

    tabsize

--- Tutorial/toDoList/script.js
+++ Tutorial/toDoList/script.js
@@ -3,44 +3,44 @@ const taskList = document.getElementById("tasks");
 
 //adding enter as a way to submit a list
 document.querySelector("input").addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-        const inputField = document.getElementById("task-input");
-        if (!(inputField.value === "")) {
-            addTask(inputField.value);
-            form.reset();
-        }
+  if (event.key === "Enter") {
+    const inputField = document.getElementById("task-input");
+    if (!(inputField.value === "")) {
+      addTask(inputField.value);
+      form.reset();
     }
+  }
 });
 
 form.onsubmit = function (e) {
-    e.preventDefault();
-    const inputField = document.getElementById("task-input");
-    // console.log(inputField.value);
-    if (!(inputField.value === "")) {
-        addTask(inputField.value);
-        form.reset();
-    }
+  e.preventDefault();
+  const inputField = document.getElementById("task-input");
+  // console.log(inputField.value);
+  if (!(inputField.value === "")) {
+    addTask(inputField.value);
+    form.reset();
+  }
 };
 
 function addTask(description) {
-    const taskContainer = document.createElement("div");
-    const newTask = document.createElement("input");
-    const taskLabel = document.createElement("label");
-    const taskDecriptionNode = document.createTextNode(description);
+  const taskContainer = document.createElement("div");
+  const newTask = document.createElement("input");
+  const taskLabel = document.createElement("label");
+  const taskDecriptionNode = document.createTextNode(description);
 
-    newTask.setAttribute("type", "checkbox");
-    newTask.setAttribute("name", description);
-    newTask.setAttribute("id", description);
+  newTask.setAttribute("type", "checkbox");
+  newTask.setAttribute("name", description);
+  newTask.setAttribute("id", description);
 
-    taskLabel.setAttribute("for", description);
-    taskLabel.appendChild(taskDecriptionNode);
+  taskLabel.setAttribute("for", description);
+  taskLabel.appendChild(taskDecriptionNode);
 
-    taskContainer.classList.add("task-item");
-    taskContainer.appendChild(newTask);
-    taskContainer.appendChild(taskLabel);
+  taskContainer.classList.add("task-item");
+  taskContainer.appendChild(newTask);
+  taskContainer.appendChild(taskLabel);
 
-    taskList.appendChild(taskContainer);
+  taskList.appendChild(taskContainer);
 
-    console.log(taskContainer);
-    // taskLabel.style.paddingLeft = "10px";
+  console.log(taskContainer);
+  // taskLabel.style.paddingLeft = "10px";
 }
